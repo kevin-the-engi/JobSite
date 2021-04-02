3e361f2e9d2fe41c3d1e3ca18757dc3de4d4bedb
Daniel Kosykh <dankosykh@gmail.com>
2021-04-02 11:11:49 -0700

    init file structure

---
+++ client/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Yo it rendered!</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'client/dist')
+  },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: path.join(__dirname, 'client/src'),
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[hash]-[name].[ext]',
+            },
+          }
+        ]
+      },
+      {
+        test: /\.(css)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { modules: true }
+          }
+        ],
+      }
+    ]
+  }
+}
+
+
