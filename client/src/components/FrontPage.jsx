8dd4ee7eb2d64c69d547217a5e7c9e55ea0addd6
Ricky Zhao <zrickyzhao@gmail.com>
2021-04-03 11:25:25 -0700

    improved styling of page

--- client/src/components/FrontPage.jsx
+++ client/src/components/FrontPage.jsx
@@ -2,10 +2,9 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const FlexDiv = styled.div`
+  font-family: Arial;
   display: flex;
   margin: -8px;
-  padding-top: 24px;
-  padding-left: 36px;
   height: 100vh;
   width: 100vw;
   justify-content: center;
@@ -20,44 +19,60 @@ const Div = styled.div`
   margin-top: 50vh;
   margin-left: 20vw;
   padding: 24px;
-  border: 4px solid grey;
   border-radius: 8px;
   float: left;
 `;
 const Form = styled.form`
-  height: 70%;
-  width: 30%;
+  height: 65%;
+  width: 35%;
   min-width: 256px;
   max-width: 320px;
   margin: auto;
   padding: 36px;
-  border: 4px solid grey;
-  border-radius: 8px;
+  border-radius: 5px;
   position: relative;
   float: right;
   text-align: center;
-  background: white;
+  background: rgba(255,255,255,0.95);
+  box-shadow: 8px 12px 16px #333;
 `;
 const Input = styled.input`
   height: 36px;
   width: 90%;
-  border: 2px solid grey;
-  border-radius: 8px;
+  border: 1px solid grey;
+  border-radius: 5px;
 `;
 const Label = styled.label`
   float: left;
   margin: 25px 25px 5px 25px;
 `;
 const Button = styled.button`
-  height: 36px;
-  width: 90%;
-  border: 2px solid grey;
-  border-radius: 8px;
+  height: 40px;
+  width: 55%;
+  margin: 45px auto;
+  border: 1px solid grey;
+  border-radius: 25px;
+  background: white;
+`;
+const BottomDiv = styled.div`
+  position: absolute;
+  bottom: 0;
+  width: 80%;
+  margin: auto auto 45px auto;
+  text-align: center;
 `;
 
 const FrontPage = () => {
   const [user, setUser] = useState('seeker');
   const [formType, setFormType] = useState('Register');
+
+  const formToggle = () => {
+    if (formType === 'Register') {
+      setFormType('Login');
+    } else {
+      setFormType('Register');
+    }
+  };
 
   return (
     <FlexDiv>
@@ -68,15 +83,42 @@ const FrontPage = () => {
       <Form>
         <h2>{formType}</h2>
         <h4>Looking for a job? Sign up now!</h4>
-        <Label htmlFor="firstName">{'First Name: '}</Label>
-        <Input type="text" id="firstName" />
-        <Label htmlFor="lastName">{'Last Name: '}</Label>
-        <Input type="text" id="lastName" />
+        {
+          formType === 'Register' ? (
+            <>
+              <Label htmlFor="firstName">{'First Name: '}</Label>
+              <Input type="text" id="firstName" />
+              <Label htmlFor="lastName">{'Last Name: '}</Label>
+              <Input type="text" id="lastName" />
+            </>
+          ) : (
+            <>
+              <br />
+              <br />
+            </>
+          )
+        }
         <Label htmlFor="email">{'Email: '}</Label>
         <Input type="email" id="email" />
         <Label htmlFor="password">{'Password: '}</Label>
         <Input type="password" id="password" />
-
+        <br />
+        <Button type="submit">{formType}</Button>
+        {
+          formType === 'Register'
+            ? (
+              <BottomDiv>
+                {'Already a user? '}
+                <a href="#app" onClick={formToggle}>Sign in</a>
+              </BottomDiv>
+            )
+            : (
+              <BottomDiv>
+                {'Not yet a user? '}
+                <a href="#app" onClick={formToggle}>Register</a>
+              </BottomDiv>
+            )
+        }
       </Form>
     </FlexDiv>
   );
