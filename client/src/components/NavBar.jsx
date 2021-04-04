3e7d2e75b68e5c7bb92f79b69723fd2a069be31c
Jacob Peterson <petersonjacobw@gmail.com>
2021-04-04 16:36:09 -0700

    tweaks to nav slogan size responsiveness

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -5,33 +5,39 @@ import styled from 'styled-components';
 const NavBarWrapper = styled.div`
   display: flex;
   align-items: flex-end;
-  width: 101%;
-  max-width: 101%;
+  justify-content: center;
+  width: 100vw;
   height: 6vh;
   background: #fff;
   margin: -10px 0 1vh 0;
-  padding: 0 1vw 0 5vw;
   border-bottom: 1px solid #e0e0e0;
   color: #000;
   font-family: Arial, sans-serif;
 
   @media (min-width: 768px) {
     margin: -10px 0 3vh 0;
+    justify-content: left;
   }
 `;
 
 const Logo = styled.h1`
-  font-size: 2rem;
   margin: 0;
+  padding: 0 1vw 0 0;
+  font-size: 2.5rem;
   @media (min-width: 768px) {
+    padding: 0 1vw 0 5vw;
     font-size: 3rem;
   }
 `;
 
 const Slogan = styled.p`
-  font-size: 1rem;
-  margin: 0 0 7px 1vw;
-  font-style: italic;
+  display: none;
+  @media (min-width: 500px) {
+    display: inline;
+    font-size: 1rem;
+    margin: 0 0 7px 0;
+    font-style: italic;
+  }
 `;
 
 const NavBar = (props) => (
