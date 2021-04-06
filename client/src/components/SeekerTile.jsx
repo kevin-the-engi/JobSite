2a7d3f68e5660234813b84a9924724840dafbd08
Jacob Peterson <petersonjacobw@gmail.com>
2021-04-06 10:36:22 -0700

    Fixed seeker tile`

--- client/src/components/SeekerTile.jsx
+++ client/src/components/SeekerTile.jsx
@@ -15,39 +15,51 @@ const Tile = styled.div`
 `;
 
 const Name = styled.h1`
-  margin: .5vh 0 .25vh 0;
+  margin: .5vh 0 .25vh .5vw;
   font-size: 1rem;
   font-weight: bold;
 `;
 
 const Summary = styled.p`
-  margin: 0;
+  margin: 0 1vw;
   font-weight: lighter;
   font-style: italic;
 `;
 
-const Email = styled.p`
-  margin: 0;
-  font-weight: lighter;
+const RowDiv = styled.div`
+  margin: 0 1vw;
+  display: flex;
+  justify-content: left;
 `;
 
-const Degree = styled.p`
-  margin: 0;
-  font-weight: lighter;
+const SpacedRowDiv = styled.div`
+  margin: 0 .25vw 0 1vw;
+  display: flex;
+  justify-content: space-between;
 `;
 
-const Work = styled.p`
+const Text = styled.p`
   margin: 0;
-  font-weight: lighter;
+`;
+
+const ItalicText = styled.p`
+  margin: 0;
+  font-style: italic;
 `;
 
 const SeekerTile = ({ seeker, toggleModal, getResumeToDisplay }) => (
   <Tile onClick={() => { getResumeToDisplay(seeker); toggleModal(); }}>
     <Name>{`${seeker.firstName} ${seeker.lastName}`}</Name>
     <Summary>Javascript | React | Node</Summary>
-    <Email>{seeker.email}</Email>
-    <Degree>{`Degree: ${seeker.education[seeker.education.length - 1].degreeType} in ${seeker.education[seeker.education.length - 1].fieldOfStudy}`}</Degree>
-    <Work>{`Last Job: ${seeker.workExperience[seeker.workExperience.length - 1].title} @ ${seeker.workExperience[seeker.workExperience.length - 1].employer}`}</Work>
+    <RowDiv>
+      <Text>{`Education: ${seeker.education[seeker.education.length - 1].fieldOfStudy.charAt(0).toUpperCase()}${seeker.education[seeker.education.length - 1].fieldOfStudy.slice(1)}`}</Text>
+      &nbsp;
+      <ItalicText>{`(${seeker.education[seeker.education.length - 1].degreeType})`}</ItalicText>
+    </RowDiv>
+    <SpacedRowDiv>
+      <Text>{`Last Job: ${seeker.workExperience[seeker.workExperience.length - 1].title} @ ${seeker.workExperience[seeker.workExperience.length - 1].employer}`}</Text>
+      <ItalicText>{`${seeker.workExperience[seeker.workExperience.length - 1].startDate.slice(0, 4)} – ${seeker.workExperience[seeker.workExperience.length - 1].endDate.slice(0, 4)}`}</ItalicText>
+    </SpacedRowDiv>
   </Tile>
 );
 
