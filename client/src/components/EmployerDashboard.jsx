28a13a232dd2c4e5a7a299fb9374173e2e138930
Jacob Peterson <petersonjacobw@gmail.com>
2021-04-06 13:13:25 -0700

    Refactoring for merge with other routed components

---
+++ client/src/components/EmployerDashboard.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const EmployerDashboard = () => {
+  return (
+    <div>
+      <h1>Yo it rendered a dashboard!</h1>
+    </div>
+  )
+}
+
+export default EmployerDashboard;
