406102aebafbcc314048b8bc89574ee33e34b6de
Kevin Ng <kevin.the.engi@gmail.com>
2021-04-06 12:21:49 -0700

    Fix merge conflicts

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
-import NavBar from './NavBar.jsx';
-import JobPortal from './JobPortal.jsx';
-
-const AppWrapper = styled.div`
-  margin: 0 auto;
-`;
-
 const App = () => (
-  <AppWrapper>
-    <NavBar />
-    <JobPortal />
-  </AppWrapper>
+  <div>
+    <h1>Yo it rendered!</h1>
+  </div>
 );
 
 export default App;
