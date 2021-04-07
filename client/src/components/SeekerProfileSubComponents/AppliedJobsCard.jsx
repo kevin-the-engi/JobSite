7d2d8f0105649a71b2174ad7afbe0ebbc8df71a8
Kevin Ng <kevin.the.engi@gmail.com>
2021-04-06 17:03:30 -0700

    Add delete modal Co-authored-by: Jacob petersonjacobw@gmail.com Co-authored-by: Kevin kevin.the.engi@gmail.com

--- client/src/components/SeekerProfileSubComponents/AppliedJobsCard.jsx
+++ client/src/components/SeekerProfileSubComponents/AppliedJobsCard.jsx
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-const AppliedJobsCardWrapper = styled.div`
+import DeleteModal from './DeleteModal.jsx';
+
+const Wrapper = styled.div`
   width: 95%;
   height: 10vh;
   border: 2px solid yellow;
   border-radius: 10px;
 `;
 
+const ModalBackground = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 4;
+  background-color: #42424275;
+`;
+
 const AppliedJobsCard = (props) => {
   const { job: { name, company, description } } = props;
+  const [show, setShow] = useState('false');
+
+  const toggleModal = (event) => {
+    event.preventDefault();
+    setShow(!show);
+  };
+
   return(
-    <AppliedJobsCardWrapper>
+    <Wrapper onClick={toggleModal}>
       {name} {company} {description}
-    </AppliedJobsCardWrapper>
+      {!show
+        ? (
+          <ModalBackground onMouseDown={toggleModal}>
+            <DeleteModal toggleModal={toggleModal} />
+          </ModalBackground>
+        ) : null}
+    </Wrapper>
   );
 };
 
--- client/src/components/SeekerProfileSubComponents/RemindersCard.jsx
+++ client/src/components/SeekerProfileSubComponents/RemindersCard.jsx
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-const RemindersCardWrapper = styled.div`
+import DeleteModal from './DeleteModal.jsx';
+
+const Wrapper = styled.div`
   width: 95%;
   height: 10vh;
   border: 2px solid yellow;
   border-radius: 10px;
 `;
 
+const ModalBackground = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 4;
+  background-color: #42424275;
+`;
+
 const RemindersCard = (props) => {
   const { reminder: { date, type, text } } = props;
+  const [show, setShow] = useState('false');
 
-  return(
-    <RemindersCardWrapper>
+  const toggleModal = (event) => {
+    event.preventDefault();
+    setShow(!show);
+  };
+
+  return (
+    <Wrapper onClick={toggleModal}>
       {date} {type} {text}
-    </RemindersCardWrapper>
+      {!show
+        ? (
+          <ModalBackground onMouseDown={toggleModal}>
+            <DeleteModal toggleModal={toggleModal} />
+          </ModalBackground>
+        ) : null}
+    </Wrapper>
+
   );
 };
 
