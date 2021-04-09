dea8620934e5fb4fd7efa2c237a13d0d3b9efe94
Daniel Kosykh <dankosykh@gmail.com>
2021-04-09 14:54:22 -0700

    add filter functionality

--- client/src/components/SeekerProfileSubComponents/DropDown.jsx
+++ client/src/components/SeekerProfileSubComponents/DropDown.jsx
@@ -8,20 +8,20 @@ const Option = styled.option`
 `;
 
 const DropDown = ({ selectInterest }) => {
-  const [selected, setSelected] = useState('exInterested');
+  const [selected, setSelected] = useState('0');
 
   const handleSelect = (event) => {
     const { value } = event.target;
-
     setSelected(value);
     selectInterest(value);
   };
 
   return (
     <Select id="interest-select" value={selected} onChange={handleSelect}>
-      <Option value="exInterested">Extremely Interested</Option>
-      <Option value="veryInterested">Very Interested</Option>
-      <Option value="interested">Interested</Option>
+      <Option value="0" selected>Show All</Option>
+      <Option value="3">Extremely Interested</Option>
+      <Option value="2">Very Interested</Option>
+      <Option value="1">Interested</Option>
     </Select>
   );
 };
--- client/src/components/SeekerProfileSubComponents/SavedJobs.jsx
+++ client/src/components/SeekerProfileSubComponents/SavedJobs.jsx
@@ -12,65 +12,60 @@ const SavedJobsWrapper = styled.div`
   align-items: center;
 `;
 
+// const savedJobs = [
+//   {
+//     name: 'Software Developer', company: 'Facebook', description: 'description', interestLevel: '3',
+//   },
+//   {
+//     name: 'Front-end', company: 'Google', description: 'description', interestLevel: '1',
+//   },
+//   {
+//     name: 'Systems Engineer', company: 'Twitter', description: 'description', interestLevel: '1',
+//   },
+//   {
+//     name: 'Burger-tasting Expert', company: 'McDonalds', description: 'description', interestLevel: '2',
+//   },
+// ];
+
+const interests = {
+  exInterested: '3',
+  veryInterested: '2',
+  interested: '1',
+  showAll: '0',
+};
+
 const SavedJobs = (props) => {
   // const { savedJobs } = props;
-  const [jobData, setJobData] = useState(null);
-
-  const savedJobs = [
-    {
-      name: 'Software Developer', company: 'Facebook', description: 'description', interestLevel: '3',
-    },
-    {
-      name: 'Front-end', company: 'Google', description: 'description', interestLevel: '1',
-    },
-    {
-      name: 'Systems Engineer', company: 'Twitter', description: 'description', interestLevel: '1',
-    },
-    {
-      name: 'Burger-tasting Expert', company: 'McDonalds', description: 'description', interestLevel: '2',
-    },
-  ];
-  const [interestLevel, setInterestLevel] = useState('3');
+  const [jobData, setJobData] = useState([]);
+  const [interestLevel, setInterestLevel] = useState('0');
 
   useEffect(() => {
     const jobIdArray = [];
     if (props.savedJobs.length !== 0) {
-      for (const job of props.savedJobs) {
-        jobIdArray.push(job['jobListingId']);
-      }
+      props.savedJobs.forEach((job) => {
+        jobIdArray.push(job.jobListingId);
+      });
     }
-    const searchBody = {
-      data: jobIdArray,
-    };
+    const searchBody = { data: jobIdArray };
     post('api/listing/savedlistings', searchBody)
       .then((result) => {
-        console.log(result);
         setJobData(result);
       })
       .catch((err) => console.log(err));
   }, [props.savedJobs]);
 
   const selectInterest = (interest) => {
-    const interests = {
-      exInterested: '3',
-      veryInterested: '2',
-      interested: '1',
-    };
-
-    setInterestLevel(interests[interest]);
+    setInterestLevel(interest);
   };
 
   return (
     <SavedJobsWrapper>
       <DropDown selectInterest={selectInterest} />
-      {props.savedJobs.map((job) => (
-        (job.interestLevel === interestLevel
-          ? (
-            <SavedJobsCard
-              job={job}
-            />
-          ) : null)
-      ))}
+      {interestLevel === '0'
+        ? props.savedJobs.map((job) => (<SavedJobsCard job={job} />))
+        : props.savedJobs
+          .filter((job) => job.interestLevel === interestLevel)
+          .map((job) => (<SavedJobsCard job={job} />))}
     </SavedJobsWrapper>
   );
 };
